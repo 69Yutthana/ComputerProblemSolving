426d2d3d839e80fdf26324095c1aa62f515d043e
69Yutthana <694259018@webmail.npru.ac.th>
2026-07-08 16:56:30 +0700

    TWB.java

---
+++ week5/TWB.java
@@ -0,0 +1,16 @@
+void main() {
+    IO.println("Enter score 1: ");
+    double S1 = Double.parseDouble(IO.readln());
+    IO.println("Enter score 2: ");
+    double S2 = Double.parseDouble(IO.readln());
+    IO.println("Enter score 3: ");
+    double S3 = Double.parseDouble(IO.readln());
+    IO.println("Enter bonus : ");
+    double Bonus = Double.parseDouble(IO.readln());
+    double TS = S1 + S2 + S3;
+    double TWB = TS + Bonus;
+    double averageOfTwb = TS /3
+IO.println("Total score = " + TS);
+    IO.println("Total with bonus = " + TWB);
+    IO.println("Average of totalWithBonus(%.0f) = %2f " poins ) ;
+}
